7109883c8a5c16990351c24ad2843b69bbf94a39
Luca <luca24ever@gmail.com>
2023-11-30 17:51:36 +0100

    first commit

---
+++ Player.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from mfrc522 import SimpleMFRC522
+import RPi.GPIO as GPIO
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from time import sleep
+import json
+
+DEVICE_ID="YOUR_DEVICE_ID"
+CLIENT_ID="YOUR_CLIENT_ID"
+CLIENT_SECRET="YOUR_CLIENT_SECRET"
+
+try:
+    song_dict = json.load(open("songs.json", "r"))
+except:
+    print("Error loading songs.json")
+    exit()
+
+while True:
+    try:
+        reader=SimpleMFRC522()
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                                       client_secret=CLIENT_SECRET,
+                                                       redirect_uri="http://localhost:8080",
+                                                       scope="user-read-playback-state,user-modify-playback-state"))
+        
+        # create an infinite while loop that will always be waiting for a new scan
+        while True:
+            print("Waiting for record scan...")
+            id= reader.read()[0]
+            print("Card Value is:",id)
+            sp.transfer_playback(device_id=DEVICE_ID, force_play=False)
+            
+            # DONT include the quotation marks around the card's ID value, just paste the number
+            if song_dict.get(id):
+                sp.start_playback(device_id=DEVICE_ID, uris=[song_dict.get(id)])
+                sleep(2)
+            else:
+                print("Card value not present on the file data")
+    # if there is an error, skip it and try the code again (i.e. timeout issues, no active device error, etc)
+    except Exception as e:
+        print(e)
+        pass
+
+    finally:
+        print("Cleaning  up...")
+        GPIO.cleanup()
